b530d8817fa7ef99e9150dd7d99324f31e778c25
janaSunrise <warriordefenderz@gmail.com>
2022-09-25 00:12:54 +0530

    route deco done

--- src/plugin.ts
+++ src/plugin.ts
@@ -6,8 +6,14 @@ import { defaultAppMetadataValue } from './schema/AppMetadata';
 import type { AppMetadata } from './schema/AppMetadata';
 import type { PluginSettings } from './interfaces/plugin-settings';
 
+interface Route {
+  route: string;
+  method: string;
+}
+
 export abstract class Plugin {
   public readonly event: Emitter;
+  protected _routes: Route[];
 
   private metadata: AppMetadata;
 
@@ -15,10 +21,21 @@ export abstract class Plugin {
     this.metadata = { ...defaultAppMetadataValue, ...metadata };
 
     this.event = event();
+    this._routes = [];
+  }
+
+  public get routes(): Route[] {
+    return this._routes;
   }
 
   abstract get settings(): PluginSettings;
 
   abstract middlewares(app: Engine): void;
-  abstract routes(app: Engine): void;
+
+  // Route decorator
+  public route = (route: Route) => {
+    return (target: Function) => {
+      this._routes.push(route);
+    }
+  }
 }
