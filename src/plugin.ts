7ce4ca1cde4e1e8ed90213d999f9a8bbd125409f
janaSunrise <warriordefenderz@gmail.com>
2022-09-25 16:48:55 +0530

    remove decorator, have abstract handler

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,45 +1,12 @@
-import { EventEmitter, MiddlewarePosition } from 'eviate';
+import { Engine, EventEmitter } from 'eviate';
 
 import { defaultAppMetadataValue } from './schema/AppMetadata';
 
-import type { Context } from 'eviate';
 import type { AppMetadata } from './schema/AppMetadata';
 import type { PluginSettings } from './interfaces/plugin-settings';
 
-type MiddlewarePositionKeys = keyof typeof MiddlewarePosition;
-type MiddlewarePositionValues =
-  typeof MiddlewarePosition[MiddlewarePositionKeys];
-
-interface Route {
-  route: string;
-  method: string;
-  handler: (ctx: Context) => any;  // Add fine-grained type
-}
-
-interface Middleware {
-  position: string;
-  route: string;
-  handler: (ctx: Context) => any;  // Add fine-grained type
-}
-
-// Decorators for route and middleware, which are used to register routes and middlewares
-export function route(route: string, method: string) {
-  return function (target: any, _: string, descriptor: PropertyDescriptor) {
-    target._routes.push({ route, method, handler: descriptor.value });
-  };
-}
-
-export function middleware(position: MiddlewarePositionValues, route: string) {
-  return function (target: any, _: string, descriptor: PropertyDescriptor) {
-    target._middlewares.push({ position, route, handler: descriptor.value });
-  };
-}
-
 export abstract class Plugin {
   public readonly event: EventEmitter;
-
-  public _routes: Route[];
-  public _middlewares: Middleware[];
 
   private _metadata: AppMetadata;
 
@@ -47,18 +14,13 @@ export abstract class Plugin {
     this._metadata = { ...defaultAppMetadataValue, ...metadata };
 
     this.event = new EventEmitter();
-
-    this._routes = [];
-    this._middlewares = [];
-  }
-
-  public get routes(): Route[] {
-    return this._routes;
   }
 
   public get metadata(): AppMetadata {
     return this._metadata;
   }
+
+  public abstract handler(app: Engine): void;
 
   abstract get settings(): PluginSettings;
 }
